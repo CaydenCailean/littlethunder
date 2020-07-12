5dcd67b8f14927a8475eff9ee1afc6038f703da9
CaydenCailean <furius22@gmail.com>
2020-07-12 00:16:11 -0400

    crash fix for poe cog removal

--- littleThunder.py
+++ littleThunder.py
@@ -7,7 +7,6 @@ import re
 from discord.ext import commands
 from dbinit import lt_db
 from cogs.info import info
-from cogs.poe import poe
 from cogs.utility import utility
 from cogs.rpg import rpg
 from cogs.mw import mw
@@ -32,7 +31,6 @@ config = dict(cfg.items("prod"))
 
 # separate configured credentials to their respect services.
 
-twi_cred = {"accountsid": config["accountsid"], "authtoken": config["authtoken"]}
 dis_cred = {"discToken": config["discordtoken"]}
 discToken = dis_cred["discToken"]
 
@@ -45,7 +43,6 @@ lt_db.db_init()
 # add cogs before startup
 
 bot.add_cog(info(bot))
-bot.add_cog(poe(bot))
 bot.add_cog(utility(bot))
 bot.add_cog(rpg(bot, lt_db))
 bot.add_cog(mw(bot))
