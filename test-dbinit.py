d13e66e0ea45fd88b3ece0516efe1d75c5f5263f
Lance Zeligman <lance.zeligman@gmail.com>
2020-07-15 00:02:54 -0400

    Introduction of testing

---
+++ test-dbinit.py
@@ -0,0 +1,73 @@
+import pytest
+from pytest_mongodb.plugin import mongomock
+from pytest_mongodb.plugin import pymongo
+from dbinit import lt_db
+
+
+bad_config = {}
+
+good_config = {
+    "user": "test",
+    "pass": "test",
+    "host": "127.0.0.1",
+    "port": 27017,
+    "dbname": "users",
+}
+
+
+class Testdb:
+    @pytest.fixture()
+    def db(self):
+        db = lt_db(good_config)
+        return db
+
+    def test_config(self, db):
+        """
+        test_config Tests that a valid config is not altered
+
+        :param db: lt_db database object
+        :type db: lt_db
+        """
+        assert db.user == good_config["user"]
+        assert db.password == good_config["pass"]
+        assert db.host == good_config["host"]
+        assert db.port == good_config["port"]
+        assert db.dbname == good_config["dbname"]
+
+    def test_invalid_config(self):
+        """
+        test_invalid_config Tests that an invalid config raises exception
+        """
+        with pytest.raises(Exception):
+            assert lt_db(bad_config)
+
+    def test_connection(self, db):
+        """
+        test_connection [summary]
+
+        :param db: lt_db database object
+        :type db: lt_db
+        """
+        db.connect()
+        assert isinstance(db.client, pymongo.mongo_client.MongoClient)
+
+    def test_db_init(self, db):
+        """
+        test_db_init test database init
+
+        :param db: lt_db database object
+        :type db: lt_db
+        """
+
+        db.connect()
+        db.db_init()
+        assert isinstance(db.db, pymongo.database.Database)
+
+    def test_db_retrieval(self, db):
+        """
+        test_db_retrieval Begins the retrieval tasks
+
+        :param db: lt_db database object
+        :type db: lt_db
+        """
+        pass
