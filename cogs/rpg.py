ddb6ef8fb7cd19e3672338c1e0da9fe0b2193d7b
CaydenCailean <furius22@gmail.com>
2020-09-15 08:18:48 -0400

    fix

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -243,7 +243,7 @@ class rpg(commands.Cog):
         Syntax:
         .init add [name] [dice]
         """
-        dieRoll = dieRoll.lower()
+        
         Category, Guild, ID = self.ctx_info(ctx)
         try:
             int(dieRoll)
