9a5606fa0aedb4e72bc8b4c0fa0010499944269a
CaydenCailean <furius22@gmail.com>
2020-11-06 09:02:39 -0500

    initsetturn

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -339,8 +339,11 @@ class rpg(commands.Cog):
         turnNum = self.lt_db.turn_get(Guild, Category)
 
         if type(newPos) == str:
-            newPos = list(map(itemgetter('name'), initraw)).index(newPos)
-            self.lt_db.turn_set(Guild, Category, newPos)
+            try:
+                for x in initraw:
+                    if itemgetter('name')(s) == newPos:
+                        print(initraw.index(x))
+            #self.lt_db.turn_set(Guild, Category, newPos)
         if type(newPos)== int and turnNum >= newPos:
             self.lt_db.turn_set(Guild, Category, newPos)
         await ctx.send("The initiative table's state has been updated!")
