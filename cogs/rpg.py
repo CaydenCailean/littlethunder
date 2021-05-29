d4083b5e146286793a59de2546b3952a764660a0
CaydenCailean <furius22@gmail.com>
2021-05-29 13:03:45 -0400

    default roll comment

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -147,7 +147,7 @@ class rpg(commands.Cog):
                         commentText = re.search(r"#(.+)", ctx.message.content)
                         commentText = commentText.group(0).replace("#", "")
                     except:
-                        commentText = input
+                        commentText = f"Rolling {input}"
 
                 if hasattr(ctx.message.author, "nick") == True:
                     if ctx.message.author.nick != None:
