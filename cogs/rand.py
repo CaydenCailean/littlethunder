484dc9b98c03fd6c165d413c8673cf9afca2c40b
ejlam <ejlamoureux@gmail.com>
2022-01-21 17:05:40 -0500

    fix multi

--- cogs/rand.py
+++ cogs/rand.py
@@ -50,7 +50,8 @@ class rand(commands.Cog):
                     self, message, self.__class__.__name__, "random", self.ctx.author
                 )
         if ctx.invoked_subcommand != None:
-            Table = ctx.message.content.split(" ", 1)[1][:1]
+            Table = ctx.message.content.split(" ", 1)[1]
+            Table = Table[:2]
             try:
                 await self.get(ctx, Table)
             except:
