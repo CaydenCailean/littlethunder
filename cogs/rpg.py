e0c2d0795a0aebe1febe0096ae7d1d87eedc66ce
ejlam <ejlamoureux@gmail.com>
2022-05-17 00:00:12 -0400

    test

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -215,7 +215,7 @@ class rpg(commands.Cog):
         try:
             _, Guild, User = self.ctx_info(ctx)
             macroDict = self.db.dice_list(User, Guild)
-            
+
             description = "```\n"
             max_len = max([len(x) for x in macroDict.keys()])
 
